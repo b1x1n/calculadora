627aab89a94a32cab41b5c208172f1725a199190
Verónica Carbajo <veronica.carbajo.fdz@gmail.com>
2016-09-15 13:53:34 +0200

    Añadiendo los ultimos cambios a examen.js

--- examen.js
+++ examen.js
@@ -1,6 +1,55 @@
 function calcula(operacion){
-    var operando1 = document.calc.operando1.value
-    var operando2 = document.calc.operando2.value 
+    var operando1 = document.calculadora.operando1.value
+    var operando2 = document.calculadora.operando2.value 
     var result = eval(operando1 + operacion + operando2)
-    document.calc.resultado.value = result
+    document.calculadora.resultado.value = result
 }
+/**
+Función a analizar 
+function romanize(n) {
+	var
+		values = [1, 5, 10, 50, 100, 500, 1000],
+		letras = ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
+		res = [],
+		num, letra, val, pos, insert
+
+	for(var i = 6; num = values[i], letra = letras[i]; i--) {
+		// Suficientemente grande
+		if(n >= num) {
+			// Número de letras repetidas
+			var r = Math.floor(n / num); 
+
+			// Restamos el actual
+			n -= r * num; 
+
+			if(r < 4){
+				// Metemos las letras
+				while(r--){
+					res.push(letra);
+				}
+			} else {
+				// No se pueden repetir 4+ veces
+				val = res.pop(); // Última letra
+
+				// Si es el string vacío o letra == "M" (no hay anterior)
+				// usamos la letra anterior a esta
+				pos = (val ? letras.indexOf(val) : i) + 1; 
+
+				// Y si letra == "M" -> letras[pos] no existirá y usamos M
+				insert = letra + (letras[pos] || 'M'); 
+
+				// Insertamos el string
+				res.push(insert);
+			}
+		} else {
+			// Si no vamos a poner letra usamos un ""
+			// para que no afecte pop
+			res.push('');
+		}
+	}
+
+	return res.join('');
+}
+
+romanize(18); // "XVIII"
+**/
